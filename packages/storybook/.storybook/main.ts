fa940cacba30cb7069b7d2c2b0c6576da22880be
Derian Rosado <derian.rosado@alegra.com>
2022-08-29 15:56:46 -0500

    fix error any in webpackFinal

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -1,16 +1,16 @@
 module.exports = {
-    stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-    framework: '@storybook/vue3',
-    core: {
-        builder: 'storybook-builder-vite',
-    },
-    webpackFinal: async (config) => {
-        config.module.rules.push({
-            test: /\.vue$/,
-            loader: 'vue-docgen-loader',
-            enforce: 'post',
-        })
-        return config
-    },
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/vue3',
+  core: {
+    builder: 'storybook-builder-vite',
+  },
+  webpackFinal: async (config: any) => {
+    config.module.rules.push({
+      test: /\.vue$/,
+      loader: 'vue-docgen-loader',
+      enforce: 'post',
+    })
+    return config
+  },
 }
--- packages/storybook/.storybook/preview.ts
+++ packages/storybook/.storybook/preview.ts
@@ -1,10 +1,5 @@
 // import "@smile-ui/vue/styles"
-import "@alegradev/smile-ui-next/styles"
-// import { install as SmileUiNext } from '@alegradev/smile-ui-next'
-
-// import { app } from '@storybook/vue3'
-
-// app.use(SmileUiNext)
+import '@alegradev/smile-ui-next/styles'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
