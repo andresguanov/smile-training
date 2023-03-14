1b0e41637f78722069412049fd34ee866c45aeeb
Carlos Rolando Quispe Quispe <carlos.quispe.q@tecsup.edu.pe>
2023-03-13 19:38:40 -0500

    Feat: Finalizando primera revision del componente stepper

---
+++ src/components/SmStepper/SmStep.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sm-step" :class="[state]">
+    <div class="sm-step__header">
+      <span class="sm-step__line sm-step__line--left" />
+      <span class="sm-step__circle" />
+      <span class="sm-step__line sm-step__line--right" />
+    </div>
+    <p v-if="label" class="sm-step__label">{{ label }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    state: 'pending' | 'progress' | 'error' | 'completed'
+    label?: string
+  }>(),
+  { state: 'pending' }
+)
+</script>
+
+<style scoped lang="scss" src="./SmStep.scss"></style>
