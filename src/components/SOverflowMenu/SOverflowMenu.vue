6e6065fda7162f45c4346b3f5d5589be231fdf39
Carlos Rolando Quispe Quispe <carlos.quispe.q@tecsup.edu.pe>
2023-04-26 21:17:03 -0500

    feat: Empezando con dropdown

---
+++ src/components/SOverflowMenu/SOverflowMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="menuEl" class="s-menu" :style="{ top: distance }">
+    <header class="s-menu__header"></header>
+    <div class="s-meny__body"></div>
+    <footer class="s-menu__footer"></footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    /**
+     * La distancia del menú con respecto a su contenedor padre
+     */
+    distance?: string
+  }>(),
+  { distance: '0px' }
+)
+const emit = defineEmits<{
+  (event: 'clickOutside', value: PointerEvent): void
+}>()
+
+const menuEl = ref<HTMLDivElement | null>(null)
+
+onClickOutside(menuEl, event => emit('clickOutside', event))
+</script>
+
+<style lang="scss" scoped>
+.s-menu {
+  @apply absolute;
+}
+</style>
