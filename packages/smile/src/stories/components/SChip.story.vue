48f13fc8305ff860731f8e307631b0a89fb1ee49
Derian Rosado <derian.rosado@alegra.com>
2023-10-11 22:08:43 -0600

    crear documentacion para algunos componentes

---
+++ packages/smile/src/stories/components/SChip.story.vue
@@ -0,0 +1,12 @@
+<template>
+  <Story
+    title="s-chip"
+    :layout="{
+      type: 'grid',
+    }"
+  >
+    <s-chip label="Label"></s-chip>
+  </Story>
+</template>
+
+<script setup></script>
---
+++ packages/smile/src/stories/components/SDropdown.story.vue
@@ -0,0 +1,24 @@
+<template>
+  <Story title="s-dropdown">
+    <s-dropdown v-model="inputValue" v-bind="props"></s-dropdown>
+  </Story>
+</template>
+
+<script setup>
+const inputValue = ref();
+const props = {
+  label: 'Label',
+  size: 'medium',
+  loadingText: 'Cargando contenido...',
+  textKey: 'text',
+  valueKey: 'code',
+  options: [
+    { text: 'Javascript', code: 'js', icon: 'flag-3' },
+    { text: 'PHP', code: 'php', icon: 'flag-3' },
+    { text: 'Python', code: 'py', icon: 'flag-3' },
+    { text: 'C++', code: 'cc', icon: 'flag-3' },
+  ],
+  optionalText: 'Opcional',
+  maxHeight: '300px',
+};
+</script>
