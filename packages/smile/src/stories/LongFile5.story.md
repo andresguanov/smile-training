2db3ad96a894789a6d588731ac642d5d7578ca72
Derian Rosado <derian.rosado@alegra.com>
2023-10-15 22:37:22 -0600

    Agregar doc de Sradio Sstepper Stab SToogle STooltip y SWizard

---
+++ packages/smile/src/stories/LongFile5.story.md
@@ -0,0 +1,37 @@
+<div class="sidebar-r-doc">
+  <div>En esta página</div>
+  <ul>
+    <li><a href="#conflictos">Conflictos</a></li>
+  </ul>
+</div>
+
+# Actualizar Microfronts
+
+Para llevar a cabo la actualización del proyecto en el que estamos trabajando a partir del proyecto base, se requiere seguir los siguientes pasos:
+
+- `Creación de una rama desde "develop"`: Desde la rama `develop`, crear una nueva rama específica para esta actualización.
+- `Realizar un Pull del Proyecto Base`: Asegurarse de obtener la versión más actualizada del proyecto base desde el repositorio.
+
+Estos pasos garantizan una actualización efectiva y organizada del proyecto en curso a partir de la base existente.
+
+```bash
+git pull base master
+```
+
+### Conflictos
+
+Durante el proceso de actualización de dependencias, es probable que surjan conflictos. Se recomienda identificar los cambios entre la versión local y la obtenida del proyecto base.
+
+En tales situaciones, se sugiere seguir estos pasos:
+
+- Utilizar el sentido común: Asegurarse de actualizar las dependencias del proyecto local a las versiones más recientes (según lo obtenido con el comando git pull base master del proyecto base). Además, es fundamental incluir todas las dependencias previamente existentes en el proyecto local pero ausentes en la base.
+
+- Finalmente, proceder con la instalación/actualización de las dependencias pertinentes y realizar un commit que refleje los cambios locales.
+
+```bash
+npm install
+git add .
+git commit -m "merge commit"
+```
+
+Otro conflicto que puede llegar a darse, es en el archivo de configuración "vue.config.js", en el apartado `assetsDir`. El conflicto se puede dar si en su proyecto ya actualizaron el `assetsDir` para colorcar la ruta de deploy correspondiente. Inicialmente, este apartado está vacío. Al momento de hacer el deploy de su proyecto, este apartado debe contener el path de la URL donde va a ser deployado. Cuando intenten actualizar su proyecto a partir del proyecto base, se van a encontrar con un conflicto. Entonces, en el mismo, se debe mantener la config de deploy adecuada a su proyecto: por ejemplo, si su proyecto va a la URL de /items, entonces el `assetsDir` debe ser `items`.
---
+++ packages/smile/src/stories/components/SWizard.story.vue
@@ -0,0 +1,112 @@
+<template>
+  <Story title="s-wizard" auto-props-disabled>
+    <Variant title="default">
+      <template #default>
+        <s-wizard
+          v-model="initState['v-model']"
+          v-bind="initState"
+          @clickMenu="() => console.log('ClickMenu')"
+          @close="() => console.log('Close')"
+        ></s-wizard>
+      </template>
+
+      <template #controls>
+        <component
+          v-model="initState[control.key]"
+          v-for="control in controls"
+          :is="`Hst${control.input}`"
+          :title="control.key"
+          v-bind="control.props"
+        />
+      </template>
+    </Variant>
+  </Story>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue';
+import { icons } from '../config/utils/IconOptions';
+
+/**
+ * Puede usar la función render de Vue 3:
+ * @link https://vuejs.org/guide/extras/render-function.html#creating-vnodes
+ *
+ * o directamente importando el componente que desea usar:
+ * `import step1 from "components/step1"`
+ */
+const step1 = h('div', [h('p', 'Formulario 1'), h('input', { placeholder: 'User' })]);
+const step2 = h('div', [h('p', 'Formulario 2'), h('input', { placeholder: 'Email' })]);
+const step3 = h('div', [
+  h('p', 'Formulario 2'),
+  h('input', { placeholder: 'Fecha', type: 'date' }),
+]);
+
+const initState = reactive({
+  'v-model': 0,
+  backText: 'Atrás',
+  continueText: 'Continuar',
+  steps: [
+    {
+      title: 'STEP 1',
+      description: 'Descripción del step 1',
+      label: 'user',
+      components: [step1],
+    },
+    {
+      title: 'STEP 2',
+      description: 'Descripción del step 2',
+      label: 'email',
+      components: [step2],
+    },
+    {
+      title: 'STEP 3',
+      description: 'Descripción del step 3',
+      label: 'date',
+      components: [step3],
+    },
+  ],
+});
+
+const controls = [
+  {
+    key: 'v-model',
+    input: 'Number',
+    props: {},
+  },
+  {
+    key: 'hasBackButton',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'disabledBack',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'disabledNext',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'backText',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'continueText',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'isOnComponent',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'avatar',
+    input: 'Text',
+    props: {},
+  },
+];
+</script>
