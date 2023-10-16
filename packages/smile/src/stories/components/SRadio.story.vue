16eccd20a4416f16453509e4166e3feeaeafd2a9
Derian Rosado <derian.rosado@alegra.com>
2023-10-15 22:37:22 -0600

    Agregar doc de Sradio Sstepper Stab SToogle STooltip y SWizard

---
+++ packages/smile/src/stories/components/SRadio.story.vue
@@ -0,0 +1,92 @@
+<template>
+  <Story title="s-radio" auto-props-disabled>
+    <Variant title="default">
+      <template #default>
+        <s-radio v-model="initState['v-model']" v-bind="initState"></s-radio>
+      </template>
+
+      <template #controls>
+        <component
+          v-model="initState[control.key]"
+          v-for="control in controls"
+          :is="`Hst${control.input}`"
+          :title="control.key"
+          v-bind="control.props"
+        />
+      </template>
+    </Variant>
+  </Story>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue';
+import { icons } from '../config/utils/IconOptions';
+
+const initState = reactive({
+  label: 'Label',
+  orientation: 'vertical',
+  options: [
+    {
+      value: '1',
+      label: 'Opción 1',
+    },
+    {
+      value: '2',
+      label: 'Opción 2',
+    },
+    {
+      value: '3',
+      label: 'Opción 3',
+      disabled: true,
+    },
+  ],
+  optionalText: 'Opcional',
+});
+
+const controls = [
+  {
+    key: 'v-model',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'label',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'id',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'orientation',
+    input: 'Select',
+    props: { options: ['horizontal', 'vertical'] },
+  },
+  {
+    key: 'options',
+    input: 'Json',
+    props: {},
+  },
+  {
+    key: 'error',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'required',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'markType',
+    input: 'Select',
+    props: { options: ['required', 'optional'] },
+  },
+  {
+    key: 'optionalText',
+    input: 'Text',
+  },
+];
+</script>
---
+++ packages/smile/src/stories/components/SStepper.story.vue
@@ -0,0 +1,52 @@
+<template>
+  <Story title="s-stepper" auto-props-disabled>
+    <Variant title="default">
+      <template #default>
+        <s-stepper v-model="initState['v-model']" v-bind="initState"></s-stepper>
+      </template>
+
+      <template #controls>
+        <component
+          v-model="initState[control.key]"
+          v-for="control in controls"
+          :is="`Hst${control.input}`"
+          :title="control.key"
+          v-bind="control.props"
+        />
+      </template>
+    </Variant>
+  </Story>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue';
+import { icons } from '../config/utils/IconOptions';
+
+const initState = reactive({
+  'v-model': 1,
+  steps: ['Step 1', 'Step 2', 'Step 3'],
+});
+
+const controls = [
+  {
+    key: 'v-model',
+    input: 'Number',
+    props: {},
+  },
+  {
+    key: 'steps',
+    input: 'Json',
+    props: {},
+  },
+  {
+    key: 'hideLabels',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'currentStepHasError',
+    input: 'Checkbox',
+    props: {},
+  },
+];
+</script>
