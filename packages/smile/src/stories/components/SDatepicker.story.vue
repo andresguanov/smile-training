bc369167000d912c780970b1cd3b876c00cb8f86
Carlos Rolando Quispe Quispe <carlos.quispe.q@tecsup.edu.pe>
2023-10-15 20:48:44 -0500

    feat: Agregando documentacion para SDatepicker

---
+++ packages/smile/src/stories/components/SDatepicker.story.vue
@@ -0,0 +1,155 @@
+<template>
+  <Story title="s-datepicker" auto-props-disabled>
+    <Variant title="default">
+      <template #default>
+        <s-datepicker
+          v-model="value"
+          v-bind="initState"
+          @clickOption="logEvent('clickOption', { $event })"
+        />
+      </template>
+      <template #controls>
+        <template v-for="control in controls">
+          <component
+            v-model="initState[control.key]"
+            :is="`Hst${control.input}`"
+            :title="control.key"
+            v-bind="control.props"
+          />
+        </template>
+      </template>
+    </Variant>
+  </Story>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+import { logEvent } from 'histoire/client';
+
+const value = ref();
+const initState = reactive({
+  label: 'Smile datepicker',
+  size: 'medium',
+  optionalText: 'Opcional',
+  disabledDates: [new Date()],
+  sidebarOptions: [
+    {
+      id: 'first',
+      title: 'Primera opción',
+      description: 'Esta es mi primera opción',
+    },
+    {
+      id: 'today',
+      title: 'Hoy',
+    },
+  ],
+});
+
+const controls = [
+  {
+    key: 'locale',
+    input: 'Select',
+    props: { options: ['es', 'en'] },
+  },
+  {
+    key: 'format',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'placeholder',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'clearable',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'readonly',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'id',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'label',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'disabled',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'required',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'size',
+    input: 'Select',
+    props: { options: ['small', 'medium', 'large'] },
+  },
+  {
+    key: 'disabledDates',
+    input: 'Json',
+    props: {},
+  },
+  {
+    key: 'sidebarOptions',
+    input: 'Json',
+    props: {},
+  },
+  {
+    key: 'error',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'textInput',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'rangeMode',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'multiCalendars',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'autoApply',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'markType',
+    input: 'Select',
+    props: { options: ['required', 'optional'] },
+  },
+  {
+    key: 'optionalText',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'hint',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'supportiveText',
+    input: 'Text',
+    props: {},
+  },
+];
+</script>
--- packages/smile/src/stories/wrapper/GlobalWrapper.vue
+++ packages/smile/src/stories/wrapper/GlobalWrapper.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import type { Story, Variant } from 'histoire';
 import { ref } from 'vue';
 
 type AlegraTheme =
@@ -8,39 +7,103 @@ type AlegraTheme =
   | 'sm-primary-alanube'
   | 'sm-primary-alegra-next';
 
-const props = defineProps<{
-  story: Story;
-  variant?: Variant;
-}>();
-
 const dataTheme = ref<AlegraTheme>('sm-primary-alegra');
+const isControlOpen = ref(false);
 
 const changeTheme = (theme: AlegraTheme) => {
   dataTheme.value = theme;
 };
+const toggleControl = () => (isControlOpen.value = !isControlOpen.value);
 </script>
 
 <template>
   <div :sm-data-theme="dataTheme" class="global-wrapper">
     <slot />
-    <div class="">
-      <button class="">Temas</button>
-      <ul>
+    <div class="theme-control">
+      <button class="theme-control__main" title="Temas" @click="toggleControl()" />
+      <ul class="theme-control__options" :class="{ open: isControlOpen }">
         <li>
-          <button class="" @click="changeTheme('sm-primary-alegra')">Alegra</button>
+          <button
+            class="theme-control__alegra"
+            title="Alegra"
+            @click="changeTheme('sm-primary-alegra')"
+          />
         </li>
         <li>
-          <button class="" @click="changeTheme('sm-primary-alegra-next')">Alegra Next</button>
+          <button
+            class="theme-control__alegra-next"
+            title="Alegra Next"
+            @click="changeTheme('sm-primary-alegra-next')"
+          />
         </li>
         <li>
-          <button class="" @click="changeTheme('sm-primary-alanube')">Alanube</button>
+          <button
+            class="theme-control__alanube"
+            title="Alanube"
+            @click="changeTheme('sm-primary-alanube')"
+          />
         </li>
         <li>
-          <button class="" @click="changeTheme('sm-primary-smile')">
-            Smile (solo para pruebas)
-          </button>
+          <button
+            class="theme-control__smile"
+            title="Smile"
+            @click="changeTheme('sm-primary-smile')"
+          />
         </li>
       </ul>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.theme-control {
+  @apply fixed bottom-3 right-3;
+  @apply flex items-center justify-center;
+
+  button {
+    @apply rounded-full border border-solid border-slate-600;
+    @apply relative flex items-center justify-center;
+
+    &:hover {
+      &::after {
+        @apply opacity-100;
+      }
+    }
+    &::after {
+      content: attr(title);
+      @apply bg-slate-900 text-white text-xs;
+      @apply p-1 rounded-md absolute right-full mr-1;
+      @apply whitespace-nowrap opacity-0;
+    }
+  }
+  &__main {
+    @apply bg-sm-primary-600 w-12 h-12;
+  }
+  &__options {
+    @apply opacity-0 -z-10 transition-opacity;
+    @apply absolute bottom-12 mb-1 space-y-1;
+
+    &.open {
+      @apply opacity-100 z-10;
+    }
+  }
+  &__alegra,
+  &__alegra-next,
+  &__alanube,
+  &__smile {
+    @apply w-8 h-8;
+  }
+  &__alegra {
+    background-color: rgb(10, 158, 154);
+  }
+  &__smile {
+    background-color: rgb(184, 80, 30);
+  }
+  &__alanube {
+    background-color: rgb(115, 111, 226);
+  }
+  &__alegra-next {
+    background-color: rgb(48, 171, 169);
+  }
+}
+</style>
