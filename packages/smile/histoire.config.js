7bd92272d45e1b89b263007ebce0b9f141482f86
Derian Rosado <derian.rosado@alegra.com>
2023-10-16 11:34:29 -0600

    agregar outDir al histoire

--- packages/smile/histoire.config.js
+++ packages/smile/histoire.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'histoire';
 import { HstVue } from '@histoire/plugin-vue';
 
 export default defineConfig({
+  outDir: '../../dist-docs',
   plugins: [HstVue()],
   setupFile: '/src/stories/config/histoire.setup.js',
   theme: {
