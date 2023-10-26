52d12a15ccc201d4a584e08a7b09c9b311ba4916
Carlos Rolando Quispe Quispe <carlos.quispe.q@tecsup.edu.pe>
2023-10-26 17:50:53 -0500

    feat: Agregando documentacion para STable y SmIcon

---
+++ packages/smile/src/stories/components/SmIcon.story.vue
@@ -0,0 +1,77 @@
+<template>
+  <Story title="sm-icon" auto-props-disabled>
+    <Variant title="default">
+      <template #default>
+        <sm-icon v-bind="initState"></sm-icon>
+      </template>
+      <template #controls>
+        <template v-for="control in controls">
+          <component
+            v-model="initState[control.key]"
+            :is="`Hst${control.input}`"
+            :title="control.key"
+            v-bind="control.props"
+          />
+        </template>
+      </template>
+    </Variant>
+  </Story>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+import { icons } from '../config/utils/IconOptions';
+
+const initState = reactive({
+  size: 'medium',
+  icon: 'help',
+});
+
+const controls = [
+  {
+    key: 'icon',
+    input: 'Select',
+    props: { options: icons },
+  },
+  {
+    key: 'size',
+    input: 'Select',
+    props: { options: ['small', 'medium', 'large', 'huge'] },
+  },
+  {
+    key: 'type',
+    input: 'Select',
+    props: { options: ['primary', 'secondary', 'error'] },
+  },
+  {
+    key: 'hoverColor',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'disabled',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'fluid',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'width',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'height',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'color',
+    input: 'Text',
+    props: {},
+  },
+];
+</script>
