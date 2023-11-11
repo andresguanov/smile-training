f83301cf3bd5dcc8c905bd4b07aa77dbf3856d37
Carlos Rolando Quispe Quispe <carlos.quispe.q@tecsup.edu.pe>
2023-11-10 19:52:02 -0500

    feat: Nueva prop leadingIcon para STag

--- packages/smile/src/components/STag/STag.vue
+++ packages/smile/src/components/STag/STag.vue
@@ -1,13 +1,17 @@
 <template>
   <div :class="['sm-tag-next', type]">
-    {{ label }}
+    <sm-icon v-if="leadingIcon" :icon="leadingIcon" height="12px" width="12px" />
+    <span>{{ label }}</span>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { IconType } from '../../interfaces';
+
 withDefaults(
   defineProps<{
     type?: 'primary' | 'slate' | 'rose' | 'amber' | 'green' | 'indigo';
+    leadingIcon?: IconType;
     label: string;
   }>(),
   {
