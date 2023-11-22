1cc192ce4935d8c49208a923e38fd1350fb85f82
Derian Rosado <derian.rosado@alegra.com>
2023-11-22 15:06:41 -0600

    agregar slot de input

--- packages/smile/src/components/SSlider/SSlider.vue
+++ packages/smile/src/components/SSlider/SSlider.vue
@@ -1,34 +1,40 @@
 <template>
-  <div class="s-slider">
-    <p class="s-slider-label">{{ label }}</p>
-    <div class="s-slider-content" @mouseover="showTooltip = true" @mouseout="showTooltip = false">
-      <div class="s-slider-value">
-        <transition name="fade">
-          <div
-            v-if="showTooltip"
-            class="s-slider-tooltip"
-            :style="{
-              left: `${-(sliderValue?.offsetWidth / 2) + (data / 100) * slider?.offsetWidth}px`,
-            }"
-          >
-            <span ref="sliderValue">{{ data }}</span>
-            <div class="s-slider-tooltip-arrow"></div>
-          </div>
-        </transition>
+  <div class="s-slider-container">
+    <div class="s-slider">
+      <p class="s-slider-label">{{ label }}</p>
+      <div class="s-slider-content" @mouseover="showTooltip = true" @mouseout="showTooltip = false">
+        <div class="s-slider-value">
+          <transition name="fade">
+            <div
+              v-if="showTooltip"
+              class="s-slider-tooltip"
+              :style="{
+                left: `${-(sliderValue?.offsetWidth / 2) + (data / 100) * slider?.offsetWidth}px`,
+              }"
+            >
+              <span ref="sliderValue">{{ data }}</span>
+              <div class="s-slider-tooltip-arrow"></div>
+            </div>
+          </transition>
+        </div>
+        <input
+          v-model="data"
+          type="range"
+          :min="min"
+          :max="max"
+          class="s-slider-input"
+          ref="slider"
+        />
       </div>
-      <input
-        v-model="data"
-        type="range"
-        :min="min"
-        :max="max"
-        class="s-slider-input"
-        ref="slider"
-      />
+
+      <div class="s-slider-footer">
+        <span>min</span>
+        <span>max</span>
+      </div>
     </div>
 
-    <div class="s-slider-footer">
-      <span>min</span>
-      <span>max</span>
+    <div class="s-slider-sinput">
+      <slot></slot>
     </div>
   </div>
 </template>
