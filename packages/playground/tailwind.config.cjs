ba4ef375f1af20af1ec907b78f51fa62b056f324
Bernardo Bernal <bernardobernal@outlook.es>
2024-06-12 14:27:12 -0600

    fix: Ajuste exportación de theme tailwind

--- packages/playground/tailwind.config.cjs
+++ packages/playground/tailwind.config.cjs
@@ -1,4 +1,4 @@
-const { theme } = require('@alegradev/smile-ui-next/theme');
+const { theme } = require('@alegradev/smile-ui-next');
 const colors = require('tailwindcss/colors');
 const plugin = require('tailwindcss/plugin');
 const scrollbar = require('tailwind-scrollbar');
@@ -6,7 +6,5 @@ const scrollbar = require('tailwind-scrollbar');
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './src/App.vue'],
   theme: theme(colors),
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
+  plugins: [require('tailwind-scrollbar')],
 };
