10f3c3c35d0ee7a5366ca5eeedd51c3771e473dd
FJosseph <aguadolfrank@gmail.com>
2024-07-26 11:41:00 -0500

    feat: Add supportive text and icon between items

--- packages/smile/src/interfaces/s-stats.interface.ts
+++ packages/smile/src/interfaces/s-stats.interface.ts
@@ -3,6 +3,8 @@ import type { IconType } from './sm-icon.interface';
 export interface sStatItem {
   icon?: IconType;
   trend?: number;
+  trendText?: string;
+  countIcon?: IconType;
   help?: string;
   label: string;
   value: string;
