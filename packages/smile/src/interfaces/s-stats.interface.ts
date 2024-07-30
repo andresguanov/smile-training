9289744bf2b1ef5ba9a209c5dd764f55c715f39c
FJosseph <aguadolfrank@gmail.com>
2024-07-30 16:51:34 -0500

    feat: Remove property in sStatItem interface

--- packages/smile/src/interfaces/s-stats.interface.ts
+++ packages/smile/src/interfaces/s-stats.interface.ts
@@ -5,7 +5,6 @@ export interface sStatItem {
   icon?: IconType;
   trend?: number;
   trendText?: string;
-  countIcon?: IconType;
   supportive?: string;
   supportiveIcon?: IconType;
   help?: string;
