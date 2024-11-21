9e63c076932ca2a4e83c0851239d3dd768c3f38c
andresguanov <andresguanov@gmail.com>
2024-11-21 16:13:03 -0500

    feature/AS-1208748348459027-reto-nuevo-component

---
+++ packages/smile/src/components/SNewComponent/SNewComponent.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import SModal from '../SModal/SModal.vue';
+
+const props = defineProps<{
+  modelValue: boolean;
+  cancelText?: string;
+  showCloseIcon?: boolean;
+  successText?: string;
+  headerText?: string;
+  srcImage?: string;
+  altImage?: string;
+  contentText?: string;
+}>()
+
+</script>
+
+<template>
+  <SModal
+  :modelValue="props.modelValue"
+  :showCloseIcon="props.showCloseIcon"
+  :cancelText="props.cancelText"
+  :successText="props.successText"
+>
+  <section class="s-new-component">
+    <picture>
+      <img v-if="srcImage" :src="props.srcImage" :alt="props.altImage" />
+      <img v-else src="https://via.placeholder.com/500x250.jpg" alt="Imagen sin contenido" />
+    </picture>
+    <h3>{{ props.headerText }}</h3>
+    <p>
+      {{ props.contentText }}
+    </p>
+  </section>
+  
+</SModal>
+</template>
+
+<style lang="scss" scoped>
+.s-new-component {	
+  @apply my-4 space-y-5 rounded-md;
+
+  picture {
+    @apply aspect-video;
+
+    img {
+      @apply w-full object-cover rounded-md;
+    }
+  }
+
+  h3 {
+    @apply text-2xl font-bold text-balance;
+  }
+}
+</style>
---
+++ packages/smile/src/stories/components/SNewComponent.story.vue
@@ -0,0 +1,96 @@
+<docs lang="md">
+  ## SModal
+  
+  Este componente permite mostrar un modal con un contenido personalizado, además de permitir
+  personalizar el header, footer y otros aspectos del modal.
+  
+  ### Props
+  
+  - `modelValue`: Controla la visibilidad del modal.
+  - `cancelText`: El texto del botón de cancelación.
+  - `showCloseIcon`: Determina si se muestra el icono de cerrar.
+  - `successText`: El texto del botón de éxito.
+  - `headerText`: El texto del encabezado del modal.
+  - `srcImage`: URL de la imagen a mostrar en el modal.
+  - `altImage`: Texto alternativo para la imagen.
+  - `contentText`: El texto del contenido del modal.
+</docs>
+
+<template>
+  <Story title="SNewComponent" auto-props-disabled>
+    <Variant title="default">
+      <template #default>
+        <SNewComponent
+          v-bind="initState"
+          @close="logEvent('close')"
+          @success="logEvent('success')"
+          @clickOutside="logEvent('clickOutside')"
+          @back="logEvent('back')"
+        />
+      </template>
+
+      <template #controls>
+        <component
+          v-model="initState[control.key]"
+          v-for="control in controls"
+          :is="`Hst${control.input}`"
+          :title="control.key"
+          v-bind="control.props"
+        />
+      </template>
+    </Variant>
+  </Story>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+const initState = reactive({
+  modelValue: true,
+  headerText: '[Modal Title]',
+  cancelText: 'Cancelar',
+  successText: 'Guardar',
+  contentText: 'Contenido del Modal',
+  srcImage: 'https://via.placeholder.com/500x250.jpg',
+  altImage: 'Imagen de ejemplo',
+});
+
+const controls = [
+  {
+    key: 'modelValue',
+    input: 'Checkbox',
+    props: {},
+  },
+  {
+    key: 'headerText',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'cancelText',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'successText',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'srcImage',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'altImage',
+    input: 'Text',
+    props: {},
+  },
+  {
+    key: 'contentText',
+    input: 'Text',
+    props: {},
+  },
+];
+
+</script>
